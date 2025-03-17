3aa454171feca44cb1a2a8ba757a0c7150ba47a8
Volodymyr1910 <priestvolodya@gmail.com>
2025-03-17 19:30:51 +0100

    basic dynamic routing inserted

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import ShowAllElements from './components/showAllElements/ShowAllElements'
+import { offersListForGuest } from './test data/Offer'
+import Layout from './layout/Layout';
+
+import Main from './components/main/main';
+import LoginForm from './components/loginForm/LoginForm';
+import NoPage from './components/noPage/NoPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
+  {/*<> <ShowAllElements array={offersListForGuest}/> </>*/}
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    
+
+    
+    <HashRouter>
+
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+      
+      <Route path='/' element={<Main/>} />
+
+      <Route path='sign-in-form' element={<LoginForm/>} />
+      <Route path='log-in-form' element={<LoginForm/>} />
+      
+
+      <Route path='*' element={<NoPage/>} />
+
+    </Route>
+    </Routes>
+
+
+    </HashRouter>
+    
+      
+    
+  );
 }
 
 export default App
---
+++ src/Components/noPage/NoPage.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+
+export default function NoPage() {
+  return (
+    <>
+    <div><h2>No such page... </h2></div>
+    <Link to="/">Go Home 🐝</Link>
+    </>
+  )
+}
