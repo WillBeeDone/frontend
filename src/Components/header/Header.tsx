592f9f7012e12fba5e39efa1e7ed4367fa60a334
LutsDM <lutsdm@gmail.com>
2025-03-19 21:56:46 +0100

    Resolve merge conflict in Header.tsx

--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -1,14 +1,8 @@
 import { JSX } from "react";
 import style from "./Header.module.css";
 import { NavLink } from "react-router-dom";
-<<<<<<< HEAD
-import GetCities from "../citySelector/GetCities";
-import MyButton from "../myButton/MyButton";
-import DropDown from "../dropDown/DropDown";
-=======
 import DropDown from "../dropDown/DropDown";
 import {useOffers} from "../context/OffersContext"
->>>>>>> ff9ee19b106022572dcda0e3c51c71dfd421fea7
 
 interface ILink {
   text: React.ReactNode;
