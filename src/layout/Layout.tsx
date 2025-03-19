3d2d80bb86c74ccff663e935d32f5c36bed0dde2
LutsDM <lutsdm@gmail.com>
2025-03-19 01:35:47 +0100

    homepage style

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,10 +7,23 @@ import Header from "../components/header/Header"
 
 
 export default function Layout() {
-  const headerElements = [{text:"Home 🏠", path:"/"}, {text:"Lessons", path:"lessons"}, {text:"Homeworks", path:"homeworks"}, {text:"cart", path:"cart"}, {text:"Products", path:"product"}, {text:"Перемикач теми", path:"themeToggle"}]
+  // const headerElements = [
+  //   { text: "Home 🏠", path: "/" },
+  //   { text: "Lessons", path: "lessons" },
+  //   { text: "Homeworks", path: "homeworks" },
+  //   { text: "cart", path: "cart" },
+  //   { text: "Products", path: "product" },
+  //   { text: "Переключатель теми", path: "themeToggle" },
+  // ];
 
-
-  const headerElementsGuest = [{text:"LOGO 🐝", path:"/"}, {text:"Sign In", path:"sign-in-form"}, {text:"Log In", path:"log-in-form"}]
+  const headerElementsGuest = [
+    { 
+      text: <img src="/logo WillBeeDone 1.png" alt="Logo" style={{ width: '140px', height: 'auto' }} />, 
+      path: "/" 
+    },
+    { text: "Sign In", path: "sign-in-form" },
+    { text: "Log In", path: "log-in-form" },
+  ];
 
   
 
@@ -18,6 +31,13 @@ export default function Layout() {
     <>
     <div className={style.boxForHeader}>
       <Header links={headerElementsGuest} />
+    </div>
+
+    <div className={style.boxForBanner}>
+      <img src="/public/587 1.png" alt="" />
+      <img src="/public/girl 1.png" alt="" />
+      <img src="/public/portrait-young-beautiful 1.png" alt="" />
+      <img src="/public/pngkit_cleaning-lady-png_555943 1.png" alt="" />
     </div>
 
     <main className={style.main}>  
