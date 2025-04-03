dd152e5e620156cf11cb7221a090de650370d5a6
Volodymyr1910 <priestvolodya@gmail.com>
2025-04-03 19:57:42 +0200

    test

--- src/components/offer/OfferCard.tsx
+++ src/components/offer/OfferCard.tsx
@@ -1,8 +1,7 @@
 import { useOffers } from "../../context/OffersContext";
 import { JSX } from "react";
 import ShowAll from "../showAll/ShowAll";
-import MyButton from "../myButton/MyButton";
-import styles from "./OfferCard.module.css";
+
 
 export default function OfferCard(): JSX.Element {
   const { offerCards } = useOffers();
