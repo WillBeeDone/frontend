503e14a3e9d2e14afd27a1830777cee4a5a7dee4
LutsDM <lutsdm@gmail.com>
2025-04-07 13:40:15 +0200

    done2

--- src/components/signOut/SignOut.tsx
+++ src/components/signOut/SignOut.tsx
@@ -2,7 +2,8 @@ import { JSX } from "react";
 import styles from "./SignOut.module.css";
 import { useDispatch } from "react-redux";
 import { signOut } from "../../features/auth/authSlice";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useOffers } from "../../context/OffersContext";
 
 interface ISignOutProps {
   onSignOut: () => void;
@@ -10,12 +11,18 @@ interface ISignOutProps {
 
 export default function SignOut({ onSignOut }: ISignOutProps): JSX.Element {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { setSelectedCategory, setSelectedKeyWord, fetchOffers } = useOffers();
 
   const handleSignOut = () => {
     dispatch(signOut());
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
     onSignOut(); 
+    setSelectedCategory("all");
+    setSelectedKeyWord("");
+    fetchOffers("all", "all", "");
+    navigate("/");
   };
 
   return (
